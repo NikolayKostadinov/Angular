1dcb872006c19bc1c09845a304f0b4b10a5c89fb
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2020-10-22 09:28:44 +0300

    Chapter8

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
-import { StoreModule } from "./store/store.module";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { StoreComponent } from './store/store.component';
+import { StoreModule } from './store/store.module';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 @NgModule({
+	imports: [
+		BrowserModule,
+		StoreModule,
+		RouterModule.forRoot([
+			{ path: 'store', component: StoreComponent, canActivate: [ StoreFirstGuard ] },
+			{ path: "cart", component: CartDetailComponent, canActivate: [ StoreFirstGuard ]},
+			{ path: "checkout", component: CheckoutComponent, canActivate: [ StoreFirstGuard ] },
+			{ path: "**", redirectTo: "/store" }
+		])
+	],
+	providers: [ StoreFirstGuard ],
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule, StoreModule ],
-	providers: [],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
